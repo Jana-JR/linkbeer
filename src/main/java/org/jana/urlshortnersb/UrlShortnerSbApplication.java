f693fedfadc05b8ffec6243388ecb0f94b11eb92
Janarthanan SKR <134406727+Jana-JR@users.noreply.github.com>
2025-05-28 15:30:29 +0530

    initial commit

---
+++ src/main/java/org/jana/urlshortnersb/UrlShortnerSbApplication.java
@@ -0,0 +1,13 @@
+package org.jana.urlshortnersb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortnerSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortnerSbApplication.class, args);
+    }
+
+}
